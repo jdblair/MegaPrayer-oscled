3b5638b2fd33bb90b6f5ceb6950cbe05a3d19ef0
John D. Blair <jdb@moship.net>
2017-03-23 22:07:38 -0700

    too long since last commit current version implements an interactive python shell that lets me manipulate led "effects" as they run the effects run on a set of beads i've implemented some sine wave effects and a few other simple effects the first effect, snakes, is broken

--- python/mp.py
+++ python/mp.py
@@ -24,12 +24,6 @@ class Color:
     def __repr__(self):
          return "Color({}, {}, {}, {})".format(self.r, self.g, self.b, self.a)
 
-    def set(self, color):
-        self.r = color.r
-        self.g = color.g
-        self.b = color.b
-        self.a = color.a
-
     def set(self, color, intensity=1):
         self.r = color.r * intensity
         self.g = color.g * intensity
@@ -46,12 +40,12 @@ class Bead:
         return "Bead(index={}, color={})".format(self.index, self.color)
 
 class Rosary:
-    def __init__(self):
+    def __init__(self, ip="127.0.0.1", port=5005):
         self.beads = []
         self.bgcolor = Color(0,0,0)
         self.effects = []
-        self.osc_ip = "127.0.0.1"
-        self.osc_port = 5005
+        self.osc_ip = ip
+        self.osc_port = port
         self.effect_id = 0;
         self.BEAD_COUNT=60
         self.run_mainloop = False
@@ -64,17 +58,27 @@ class Rosary:
             self.beads.append(Bead(i))
 
         # some useful predefined sets of beads
+        
+        self.Set_None = frozenset()
         self.Set_All = frozenset(self.beads)
         self.Set_Stem = frozenset(self.beads[0:4])
         self.Set_Ring = frozenset(self.beads[4:60])
-        self.Set_Half01 = frozenset(self.beads[4:32])
-        self.Set_Half12 = frozenset(self.beads[18:46])
-        self.Set_Half23 = frozenset(self.beads[32:60])
-        self.Set_Half30 = frozenset(self.beads[46:4])
+        self.Set_Eighth0 = frozenset(self.beads[4:11])
+        self.Set_Eighth1 = frozenset(self.beads[11:18])
+        self.Set_Eighth2 = frozenset(self.beads[18:25])
+        self.Set_Eighth3 = frozenset(self.beads[25:32])
+        self.Set_Eighth4 = frozenset(self.beads[32:39])
+        self.Set_Eighth5 = frozenset(self.beads[39:46])
+        self.Set_Eighth6 = frozenset(self.beads[46:53])
+        self.Set_Eighth7 = frozenset(self.beads[53:60])
         self.Set_Quadrent0 = frozenset(self.beads[4:18])
         self.Set_Quadrent1 = frozenset(self.beads[18:32])
         self.Set_Quadrent2 = frozenset(self.beads[32:46])
         self.Set_Quadrent3 = frozenset(self.beads[46:60])
+        self.Set_Half01 = frozenset(self.Set_Quadrent0.union(self.Set_Quadrent1))
+        self.Set_Half12 = frozenset(self.Set_Quadrent1.union(self.Set_Quadrent2))
+        self.Set_Half23 = frozenset(self.Set_Quadrent2.union(self.Set_Quadrent3))
+        self.Set_Half30 = frozenset(self.Set_Quadrent3.union(self.Set_Quadrent0))
         self.Set_Even_All = frozenset(self.beads[0:60:2])
         self.Set_Even_Ring = frozenset(self.beads[4:60:2])
         self.Set_Odd_All = frozenset(self.beads[1:60:2])
@@ -88,13 +92,21 @@ class Rosary:
         self.Color_Blue = Color(0,0,1)
         self.Color_Violet = Color(1,0,1)
         self.Color_Cyan = Color(0,1,1)
+
+    def register_effect(self, effect):
+        # instantiate the object so we get get the name
+        e = effect(self.Set_None)
+        self.effect_registry[e.name] = effect
         
     def add_effect(self, effect):
-        self.effects.append(effect)
         self.effect_id = self.effect_id + 1
         effect.id = self.effect_id
         effect.rosary = self
+        self.effects.append(effect)
         return self.effect_id
+
+    def add_effect_by_name(self, name, bead_set, color=Color(1,1,1)):
+        return self.add_effect(self.effect_registry[name](bead_set, color))
 
     def clear_effects(self):
         self.effects = []
@@ -144,15 +156,24 @@ class Rosary:
 
 
 class Effect:
+    def __init__(self, name, set, color=Color(1,1,1)):
+        self.name = name
+        self.bead_set = self.set_bead_set(set)
+        self.color = Color()
+        self.color.set(color)
+        self.duration = 0
 
     """convenience function for converting sets to lists"""
-    def bead_set_to_list(self, set):
+    def set_bead_set(self, set):
         beads = []
         for bead in set:
             beads.append(bead)
         beads.sort(key=lambda bead: bead.index)
-        return beads
-            
+        self.bead_list = beads
+
+    def get_name(self):
+        return self.name
+
 class Effect_Snake(Effect):
 
     def __init__(self, color, length=1, direction=1):
@@ -184,12 +205,10 @@ class Effect_Snake(Effect):
 class Effect_SineWave(Effect):
 
     def __init__(self, bead_set, color=Color(1,1,1), period=1, direction=1):
-        self.name = "sine_wave"
-        self.color = color
+        super().__init__("sine_wave", bead_set, color=color)
         self.offset = 0
         self.period = period
         self.direction = direction
-        self.bead_list = self.bead_set_to_list(bead_set)  # cache the ordered list
 
     def next(self, rosary):
         for b in (self.bead_list):
@@ -198,13 +217,99 @@ class Effect_SineWave(Effect):
         self.offset = (self.offset) + self.direction % 56
             
 
+class Effect_ThreePhaseSineWave(Effect):
+
+    def __init__(self, bead_set, color=Color(1,1,1), period=1, direction=1):
+        super().__init__("3phase_sine_wave", bead_set, color=color)
+        self.offset = 0
+        self.period = period
+        self.direction = direction
+        self.phase_r = 0
+        self.phase_g = .25
+        self.phase_b = .5
+
+    def next(self, rosary):
+        bead_count = len(self.bead_list)
+        phase_r = self.phase_r * bead_count
+        phase_g = self.phase_g * bead_count
+        phase_b = self.phase_b * bead_count
+
+        for bead in (self.bead_list):
+            bead.color.r = ((math.sin((2 * math.pi / bead_count * self.period) * (bead.index + self.offset + phase_r)) + 1) / 2) * self.color.r
+            bead.color.g = ((math.sin((2 * math.pi / bead_count * self.period) * (bead.index + self.offset + phase_g)) + 1) / 2) * self.color.g
+            bead.color.b = ((math.sin((2 * math.pi / bead_count * self.period) * (bead.index + self.offset + phase_b)) + 1) / 2) * self.color.b
+        self.offset = (self.offset) + self.direction % bead_count
+
+
+class Effect_SetColor(Effect):
+    def __init__(self, bead_set, color=Color()):
+        super().__init__("set_color", bead_set, color=color)
+
+    def next(self, rosary):
+        for bead in (self.bead_list):
+            bead.color.set(self.color)
+
+class Effect_Bounce(Effect):
+    def __init__(self, bead_set, color=Color(), direction=1):
+        super().__init__("bounce", bead_set, color=color)
+        self.direction = direction
+        if (self.direction < 0):
+            self.current = len(self.bead_list) - 1
+        else:
+            self.current = 0
+        self.last = self.current
+
+    def next(self, rosary):
+        self.bead_list[self.last].color.set(rosary.bgcolor)
+        self.current += self.direction
+        self.last = self.current
+        self.bead_list[self.current].color.set(self.color)
+        if (self.current >= (len(self.bead_list) - 1) or self.current <= 0):
+            self.direction *= -1
+
+
+class Effect_Throb(Effect):
+    def __init__(self, bead_set, color=Color()):
+        super().__init__("throb", bead_set, color=color)
+        self.x = 0.0
+
+    def next(self, rosary):
+        intensity = (math.sin(self.x * math.pi) + 1) / 2
+        for bead in (self.bead_list):
+            bead.color.set(self.color, intensity=intensity)
+        self.x += 0.05
     
 if __name__ == "__main__":
-    r = Rosary()
+    r = Rosary("192.168.100.114", 5005)
+
+    r.register_effect(Effect_SineWave)
+    r.register_effect(Effect_ThreePhaseSineWave)
+    r.register_effect(Effect_SetColor)
+    r.register_effect(Effect_Throb)
+    r.register_effect(Effect_Bounce)
+
     # r.add_effect(Effect_Snake(Color(0, 1, 0), length=2, direction=-1))
     # r.add_effect(Effect_Snake(Color(0, 1, 1), length=3))
     #r.add_effect(Effect_SineWave(Color(0, 0, 1), period=3, direction=1))
-    r.add_effect(Effect_SineWave(r.Set_Half01, color=Color(1, 1, 0), period=2, direction=-1))
-    r.add_effect(Effect_SineWave(r.Set_Half23, color=Color(0, 0, 1), period=2, direction=1))
+    #r.add_effect(Effect_SineWave(r.Set_Half01, color=Color(1, 1, 0), period=2, direction=-1))
+    #r.add_effect(Effect_SineWave(r.Set_Half23, color=Color(0, 1, 1), period=2, direction=1))
 
+#    r.add_effect(Effect_Bounce(r.Set_Ring, color=r.Color_Violet))
+#    r.add_effect(Effect_Bounce(r.Set_Ring, color=r.Color_Yellow, direction=-1))
+#    r.add_effect(Effect_Bounce(r.Set_Ring, color=r.Color_Violet))
+#    r.add_effect(Effect_Bounce(r.Set_Ring, color=r.Color_Violet))
+
+#    r.add_effect(Effect_Bounce(r.Set_Eighth0, color=r.Color_Red))
+#    r.add_effect(Effect_Bounce(r.Set_Eighth1, color=r.Color_Red))
+#    r.add_effect(Effect_Bounce(r.Set_Eighth2, color=r.Color_Red))
+#    r.add_effect(Effect_Bounce(r.Set_Eighth3, color=r.Color_Red))
+#    r.add_effect(Effect_Bounce(r.Set_Eighth4, color=r.Color_Blue, direction=-1))
+#    r.add_effect(Effect_Bounce(r.Set_Eighth5, color=r.Color_Blue, direction=-1))
+#    r.add_effect(Effect_Bounce(r.Set_Eighth6, color=r.Color_Blue, direction=-1))
+#    r.add_effect(Effect_Bounce(r.Set_Eighth7, color=r.Color_Blue, direction=-1))
+
+#    r.add_effect(Effect_ThreePhaseSineWave(r.Set_Stem | r.Set_Eighth7 | r.Set_Eighth0, Color(1, 1, 1), period=1, direction=1))
+    r.add_effect(Effect_ThreePhaseSineWave(r.Set_Odd_All, Color(1, 1, 1), period=1, direction=1))
+    r.add_effect(Effect_ThreePhaseSineWave(r.Set_Even_All, Color(1, 1, 1), period=1, direction=-1))
+            
     r.start()
