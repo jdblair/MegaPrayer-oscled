e866de488182c8ffb57a586825767b10c7978611
John D. Blair <jdb@moship.net>
2017-03-23 22:42:12 -0700

    my previous diagnosis of the failure was completely incorrect the initialization of the leds vector was correct. the problem was a race with the update_thread(), which was started in the led_interface constructor. I added a start_update_thread() method and I call it after intializing the vector in bind().
    it occurs to me now that a better design is to change th constructor for led_interface so it includes the length of the led string, then it could initialize the vector and start the update thread

--- src/OSCServer.cpp
+++ src/OSCServer.cpp
@@ -15,8 +15,7 @@ using namespace std;
 
 OSCServer::led_interface::led_interface(std::shared_ptr<IPlatformSerial> ser, int base, int len, bool reverse) :
     m_ser(ser), m_base(base), m_len(len), m_reverse(reverse) {
-    led_buf = new uint8_t[(m_len * 3) + 64];
-    t_update = std::thread(&OSCServer::led_interface::update_thread, this);
+    led_buf = new uint8_t[(m_len * 3)];
 };
 
 
@@ -149,12 +148,15 @@ int OSCServer::bind(shared_ptr<IPlatformSerial> ser, int base, int len, bool rev
     shared_ptr<led_interface> led_iface(new led_interface(ser, base, len, reverse));
     
     // initialize leds with zero-value leds
-    // led_t led(0, 0, 0);
+    for (auto i = 0; i < len; i++) {
+        led_iface->leds.push_back(led_t(0,0,0));
+    }
 
-    // for (auto i = 0; i < len; i++) {
-    //     led_iface->leds.push_back(led);
-    // }
-
+    // now start the update thread for this interface
+    // this avoid a race condition that happens if the update_thread() is started
+    // before the leds vector is initialized
+    led_iface->start_update_thread();
+    
     m_led_ifaces.push_back(led_iface);
 
     m_iface_count++;
