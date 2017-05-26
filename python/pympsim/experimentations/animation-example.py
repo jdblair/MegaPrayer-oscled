3ccac47e7ffec9f6ca777926dd883e9e7e68ba89
yunfan <yunfanandkevin@gmail.com>
2017-05-25 23:21:48 -0700

    - Did this: `git checkout KO_makes_a_python_sim -- pympsim`

---
+++ python/pympsim/experimentations/animation-example.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import time
+
+root=tk.Tk()
+canvas=tk.Canvas(root,width=400,height=400)
+canvas.pack()
+circle=canvas.create_oval(50,50,80,80,outline="white",fill="blue")
+
+def redraw():
+   canvas.after(100,redraw)
+   canvas.move(circle,5,5)
+   canvas.itemconfig(circle, fill="#999999")
+
+redraw()
+root.mainloop()
---
+++ python/pympsim/simulator.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+import math
+import time
+
+from tkinter import *
+from math import pi
+from math import cos
+from math import sin
+
+
+class App:
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.canvas = Canvas(master, width=900, height=700, borderwidth=0, highlightthickness=0, bg="black")
+
+        ###############John's fancy witchcraft ###################
+        i = 0
+        radius = 300
+        bead_radius = 10
+        stem_spacing = radius * pi * 2 / 56
+        x_offset = radius + (stem_spacing * 4) + 50
+        y_offset = 325
+        self.beads = []
+        #make the stem
+        for i in range(0, 4):
+            x = (x_offset - radius - stem_spacing * 4) + (stem_spacing * i)
+            y = y_offset
+            self.beads.append(self.canvas.create_oval(x, y, x+(2*bead_radius),y+(2*bead_radius), fill="#128192200", width=2))
+
+        #make the rest of the beads
+        for i in range(4, 60):
+            angle = (pi * 2 / 56 * (i - 4)) - pi
+            x = x_offset + radius * cos(angle)
+            y = y_offset + radius * sin(angle)
+            self.beads.append(self.canvas.create_oval(x, y, x+(2*bead_radius), y+(2*bead_radius), fill="#128192200", width=2))
+        ##########################################################
+
+        self.canvas.pack()
+
+    def updateBeads(self, beadData):
+        for bd in beadData:
+            bead = bd[0]
+            r = int(bd[1] * 255)
+            g = int(bd[2] * 255)
+            b = int(bd[3] * 255)
+            print(r,g,b)
+            tk_rgb = "#%02x%02x%02x" % (r, g, b)
+            self.canvas.itemconfig(self.beads[bead], fill=tk_rgb)
+
+
+def main(queue):
+    animation = Tk()
+    app = App(animation)
+
+    while True:
+        msg = queue.get()         # Read from the queue and do nothing
+        if (msg):
+            animation.update_idletasks()
+            animation.update()
+            #print(msg)
+            app.updateBeads(msg)
