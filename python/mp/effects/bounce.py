56b0fef8f329e8e71f04f30bdaba162e41c4df62
John D. Blair <jblair@netflix.com>
2017-06-04 22:43:49 -0700

    remove all references to self.last; we don't need this anymore

--- python/mp/effects/bounce.py
+++ python/mp/effects/bounce.py
@@ -21,14 +21,11 @@ class Bounce(effect.Effect):
             self.current = len(self.bead_list) - 1
         else:
             self.current = 0
-        self.last = self.current
 
     def next(self, rosary):
         super().next()
 
-        # self.bead_list[self.last].color.set(rosary.bgcolor)
         self.current += self.direction
-        self.last = self.current
         self.bead_list[self.current].color.set(self.color)
         if (self.current >= (len(self.bead_list) - 1) or self.current <= 0):
             self.direction *= -1
