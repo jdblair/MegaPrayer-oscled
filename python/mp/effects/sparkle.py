c58c77c700185644e94d9c86b9211917c4642448
John D. Blair <jblair@netflix.com>
2017-06-06 08:02:39 -0700

    working sparkle effect, with 2 knobs: frames: length of time the bead should be on size: number of beads to blink

--- python/mp/effects/sparkle.py
+++ python/mp/effects/sparkle.py
@@ -5,6 +5,11 @@ import random
 
 class Sparkle(effect.Effect):
     """
+    Blink one more randomly chosen beads.
+
+    knobs:
+    * frames: number of frames the bead should be illuminated
+    * size: number of beads to illuminate
     """
 
     # Wish there were a better way than requiring this every time
@@ -12,23 +17,30 @@ class Sparkle(effect.Effect):
 
     def __init__(self, bead_set, color=color.Color(), duration=None):
         super().__init__("sparkle", bead_set, color=color, duration=duration)
-        self.sparkle_time = 6
+        self.frames = 2
+        self.size = 1
         self.count = 0
-        if len(self.bead_list) == 0:
-            self.current = 0
-        else:
-            self.current = random.randint(0,len(self.bead_list) - 1)
+        self.bead_set = bead_set
+        random.seed()
 
     def next(self, rosary):
         super().next()
-        if len(self.bead_list) == 0:
-            self.current = 0
-        else:
-            self.current = random.randint(0,len(self.bead_list) - 1)
 
-        self.bead_list[self.current].color.set(self.color)
+        if self.count >= self.frames:
+            self.count = 0
+
+        if self.count == 0:
+            self.current = random.sample(self.bead_set, self.size)
+
+        for b in self.current:
+            b.color.set(self.color)
+        
         self.count += 1
 
     @dm.expose()
-    def set_direction(self, direction):
-        self.direction = direction
+    def set_size(self, size):
+        self.size = size
+
+    @dm.expose()
+    def set_frames(self, frames):
+        self.frames = frames
