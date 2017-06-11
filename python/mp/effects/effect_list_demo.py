0b94d26c3180d91de27d35bc6d029af95161c31e
John D. Blair <jblair@netflix.com>
2017-06-11 09:32:00 -0700

    experiment with changing effects over time

--- python/mp/effects/effect_list_demo.py
+++ python/mp/effects/effect_list_demo.py
@@ -1,4 +1,5 @@
 import copy
+
 from mp import color
 from mp.effects import effect
 from mp.effects import looper
@@ -18,11 +19,20 @@ class EffectListDemo(effect.Effect):
     def __init__(self, bead_set, color=color.Color(), **kwargs):
         super().__init__(name="effect_list_demo", bead_set=bead_set, color=color, **kwargs)
 
+        self.count = 0
         self.effects = effect_list.EffectList(self.rosary, self.id)
-        self.effects.add_effect_object(looper.Looper(bead_set, color=_color.Color(1, 0, 0), length=5))
+        self.looper1 = looper.Looper(bead_set, color=_color.Color(1, 0, 0), length=5, speed=0)
+        self.effects.add_effect_object(self.looper1)
         self.effects.add_effect_object(looper.Looper(bead_set, color=_color.Color(0, 1, 0), length=4, speed=-.5))
         self.effects.add_effect_object(sparkle.Sparkle(bead_set, color=_color.Color(1, 1, 1)))
+        self.speed_increment = .02
 
     def next(self):
         self.effects.next()
 
+        # looper1 will go faster and slower, increasing and decreasing length
+        if abs(self.looper1.speed) > 10:
+            # flip direction of speed increase
+            self.speed_increment *= -1
+        self.looper1.speed += self.speed_increment
+        self.looper1.length = abs(int(round(self.looper1.speed))) + 1
