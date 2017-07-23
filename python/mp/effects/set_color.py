b99cb23f93083c7489fdeebaeb0e7d8ff2c4554a
yunfan <yunfanandkevin@gmail.com>
2017-07-23 14:53:40 -0700

    - oscsend localhost 5006 /rosary/add_effect/name/set_color_unique/color/fire

--- python/mp/effects/set_color.py
+++ python/mp/effects/set_color.py
@@ -1,4 +1,4 @@
-
+import copy
 from mp.effects import effect
 from mp import color
 
@@ -14,3 +14,26 @@ class SetColor(effect.Effect):
     def next(self):
         for bead in (self.bead_list):
             bead.color.set(self.color)
+
+
+class SetColorUnique(effect.Effect):
+    """
+    Similar to SetColor, but each bead gets its own instance of the color object.
+
+    This is for dynamic colors so that each bead will act similarly, but not identically.
+    """
+
+    def __init__(self, bead_set, color=color.Color(), **kwargs):
+        super().__init__(name="set_color_unique", bead_set=bead_set, color=color, **kwargs)
+
+        # Give each bead its own copy of the color
+        self.bead_colors = [copy.copy(color) for b in bead_set]
+
+    def next(self):
+        for bead, col in zip((self.bead_list), self.bead_colors):
+
+            # effect.Effect's supernext(), which calls effect(), acts
+            # directly on self.color, which isn't being used here.
+            # Instead, we have to step the color
+            col.next()
+            bead.color.set(col)
